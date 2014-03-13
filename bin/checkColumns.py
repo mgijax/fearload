71bc80a9d416d16e537ef9f77375b479ad30ba1d
Sharon Giannatto <Sharon.Giannatto@jax.org>
2014-03-13 18:07:25 +0000

    fearload-5-1-7-2

---
+++ bin/checkColumns.py
@@ -0,0 +1,143 @@
+#!/usr/local/bin/python
+#
+#  checkColumns.py
+###########################################################################
+#
+#  Purpose:
+#
+#       This script checks that there are the correct number of columns
+#	in a file
+#
+#  Usage:
+#
+#      checkColumns.py  filename numColumns	
+#
+#      where:
+#          filename = path to the input file
+#
+#  Env Vars:
+#
+#      The following environment variables are set by the configuration
+#      files that are sourced by the wrapper script:
+#
+#  Inputs:
+#
+#  Outputs:
+#
+#  Exit Codes:
+#
+#      0:  Successful completion
+#      1:  An exception occurred
+#      2:  Discrepancy errors detected in the input files
+#  Implementation:
+#
+#  Notes:  None
+#
+###########################################################################
+
+
+import string
+import sys
+
+USAGE = 'Usage: checkColumns.py  inputFile numColumns'
+TAB = '\t'
+
+inputFile = None
+fpInput = None
+numColumns = None
+errors = 0
+
+#
+# Purpose: Validate the arguments to the script.
+# Returns: Nothing
+# Assumes: Nothing
+# Effects: Sets global variables.
+# Throws: Nothing
+#
+def checkArgs ():
+    global inputFile, numColumns
+
+    if len(sys.argv) != 3:
+        print USAGE
+        sys.exit(1)
+
+    inputFile = sys.argv[1]
+    numColumns = int(sys.argv[2])
+    return
+
+#
+# Purpose: Open the file for reading
+# Returns: Nothing
+# Assumes: Nothing
+# Effects: Sets global variables.
+# Throws: Nothing
+#
+def openFile ():
+    global fpInput
+
+    try:
+	fpInput = open(inputFile, 'r')
+    except:
+	print 'Cannot open input file: ' + inputFile
+	sys.exit(1)
+    return
+
+#
+# Purpose: check the file for proper number of columns
+# Returns: Nothing
+# Assumes: Nothing
+# Effects: Sets global variables.
+# Throws: Nothing
+#
+def checkColumns ():
+    #print 'starting checkColumns'
+    global errors
+    lineNum = 1
+    header = fpInput.readline()
+    for line in fpInput.readlines():
+	colError = 0
+	lineNum = lineNum + 1
+   	columns = string.split(line, TAB)
+	# remove newline from last column
+        last = columns[-1].strip()
+        columns[-1] = last
+	nc = len(columns) 
+	if nc < numColumns:
+	    errors = errors + 1
+	    colError = colError + 1
+	### start code for missing data in req columns
+	# If errors then wrong number of columns exists; so continue to next
+	if colError > 0:
+	    print 'Missing Column(s): %s' % (columns)
+	    continue
+	# default
+	bad = 1
+
+        # strand is optional
+        if columns[0] != '' and columns[1] != '' and columns[2] != '' and columns[4] != '' and columns[6] != '' and columns[9] != '' and columns[10] != '' and columns[11] != '':
+            bad = 0
+        if bad == 1:
+            print 'Missing Data in required column: %s' % (columns)
+	### end code for missing data in req columns
+    return
+
+#
+# Purpose: Close the files.
+# Returns: Nothing
+# Assumes: Nothing
+# Effects: Nothing
+# Throws: Nothing
+#
+def closeFile():
+    global fpInput
+
+    fpInput.close()
+    return
+
+checkArgs()
+openFile()
+checkColumns()
+closeFile()
+if errors > 0:
+    sys.exit(1)
+sys.exit(0)
---
+++ bin/fearQC.sh
@@ -0,0 +1,291 @@
+#!/bin/sh
+#
+#  fearQC.sh
+###########################################################################
+#
+#  Purpose:
+#
+#      This script is a wrapper around the process that does sanity
+#      checks for the FeaR load
+#
+#  Usage:
+#
+#      fearQC.sh  filename  
+#
+#      where
+#          filename = path to the input file
+#
+#  Env Vars:
+#
+#      See the configuration file
+#
+#  Inputs:
+#	FeaR file
+#
+#  Outputs:
+#
+#      - sanity report for the input file.
+#
+#      - Log file (${QC_LOGFILE})
+#
+#  Exit Codes:
+#
+#      0:  Successful completion
+#      1:  Fatal initialization error occurred
+#      3:  Fatal sanity errors
+#
+#  Assumes:  Nothing
+#
+#  Implementation:
+#
+#      This script will perform following steps:
+#
+#      ) Validate the arguments to the script.
+#      ) Validate & source the configuration files to establish the environment.
+#      ) Verify that the input file exists.
+#      ) Initialize the log and report files.
+#      ) Call fearQC.sh to generate the sanity/QC report.
+#
+#  Notes:  None
+#
+###########################################################################
+#
+#  Modification History:
+#
+#  Date        SE   Change Description
+#  ----------  ---  -------------------------------------------------------
+#
+#  03/11/2014  sc  Initial development
+#
+###########################################################################
+CURRENTDIR=`pwd`
+BINDIR=`dirname $0`
+
+CONFIG=`cd ${BINDIR}/..; pwd`/fearload.config
+USAGE='Usage: fearQC.sh  filename'
+
+# this is a sanity check only run, set LIVE_RUN accordingly
+LIVE_RUN=0; export LIVE_RUN
+
+#
+# Make sure an input file was passed to the script. If the optional "live"
+# argument is given, that means that the output files are located in the
+# /data/loads/... directory, not in the current directory.
+#
+if [ $# -eq 1 ]
+then
+    INPUT_FILE=$1
+elif [ $# -eq 2 -a "$2" = "live" ]
+then
+    INPUT_FILE=$1
+    LIVE_RUN=1
+else
+    echo ${USAGE}; exit 1
+fi
+
+#
+# Make sure the configuration file exists and source it.
+#
+if [ -f ${CONFIG} ]
+then
+    . ${CONFIG}
+else
+    echo "Missing configuration file: ${CONFIG}"
+    exit 1
+fi
+
+#
+# If this is not a "live" run, the output, log and report files should reside
+# in the current directory, so override the default settings.
+#
+if [ ${LIVE_RUN} -eq 0 ]
+then
+	SANITY_RPT=${CURRENTDIR}/`basename ${SANITY_RPT}`
+	QC_LOGFILE=${CURRENTDIR}/`basename ${QC_LOGFILE}`
+
+fi
+
+#
+# Initialize the log file.
+#
+LOG=${QC_LOGFILE}
+rm -rf ${LOG}
+touch ${LOG}
+
+#
+# FUNCTION: Check for lines with missing columns in input file and
+#           write the line numbers to the sanity report.
+#
+checkColumns ()
+{
+    FILE=$1         # The input file to check
+    REPORT=$2       # The sanity report to write to
+    NUM_COLUMNS=$3  # The number of columns expected in each input record
+    NUM_COLUMNS=$3  # The number of columns expected in each input record
+    echo "\nLines With Missing Columns or Data" >> ${REPORT}
+    echo "-----------------------------------" >> ${REPORT}
+    ${FEARLOAD}/bin/checkColumns.py ${FILE} ${NUM_COLUMNS} > ${TMP_FILE}
+    cat ${TMP_FILE} >> ${REPORT}
+    if [ `cat ${TMP_FILE} | wc -l` -eq 0 ]
+    then
+        return 0
+    else
+        return 1
+    fi
+}
+
+#
+# Initialize the report files to make sure the current user can write to them.
+#
+RPT_LIST="${SANITY_RPT}"
+
+for i in ${RPT_LIST}
+do
+    rm -f $i; >$i
+done
+
+#
+# Create a temporary file and make sure it is removed when this script
+# terminates.
+#
+TMP_FILE=/tmp/`basename $0`.$$
+trap "rm -f ${TMP_FILE}" 0 1 2 15
+#
+# FUNCTION: Check an input file to make sure it has a minimum number of lines.
+#
+checkLineCount ()
+{
+    FILE=$1        # The input file to check
+    REPORT=$2      # The sanity report to write to
+    NUM_LINES=$3   # The minimum number of lines expected in the input file
+
+    COUNT=`cat ${FILE} | wc -l | sed 's/ //g'`
+    if [ ${COUNT} -lt ${NUM_LINES} ]
+    then
+        #echo "\n\n**** WARNING ****" >> ${REPORT}
+        #echo "${FILE} has ${COUNT} lines." >> ${REPORT}
+        #echo "Expecting at least ${NUM_LINES} lines." >> ${REPORT}
+        return 1
+    else
+        return 0
+    fi
+}
+
+#
+# FUNCTION: Check for duplicate lines in an input file and write the lines
+#           to the sanity report.
+#
+checkDupLines ()
+{
+    FILE=$1    # The input file to check
+    REPORT=$2  # The sanity report to write to
+
+    echo "\n\nDuplicate Lines" >> ${REPORT}
+    echo "---------------" >> ${REPORT}
+    sort ${FILE} | uniq -d > ${TMP_FILE}
+    cat ${TMP_FILE} >> ${REPORT}
+    if [ `cat ${TMP_FILE} | wc -l` -eq 0 ]
+    then
+        return 0
+    else
+        return 1
+    fi
+}
+
+#
+# FUNCTION: Check correct values in column 1 ('Action') in an input file
+#           and write the lines to the sanity report.
+#
+checkActionValues ()
+{
+    FILE=$1    # The input file to check
+    REPORT=$2  # The sanity report to write to
+
+    echo "\n\nInvalid 'Action' Value(s) in File" >> ${REPORT}
+    echo "---------------" >> ${REPORT}
+
+    # extract uniq values from the action column eliminating the header line and
+    # converting to lower case
+    actions=`cat ${FILE}  | tail +2 | cut -f1 | sort | uniq | tr '[:upper:]' '[:lower:]'`
+
+    echo 'delete' >  ${TMP_FILE}
+    echo 'add' >> ${TMP_FILE}
+    
+    error=0
+    #actions="add other delete pbj"
+    # check for valid actions
+    for i in $actions
+    do
+	result=`cat ${TMP_FILE} | grep $i`
+	if [ "$result" = "" ]
+	then
+	    error=1
+	    echo $i >> ${REPORT}
+	fi
+    done
+}
+
+echo "" >> ${LOG}
+date >> ${LOG}
+echo "Run sanity checks on the input file" >> ${LOG}
+SANITY_ERROR=0
+
+#
+# Make sure the input files exist (regular file or symbolic link).
+#
+if [ "`ls -L ${INPUT_FILE} 2>/dev/null`" = "" ]
+then
+    echo "\nInput file does not exist: ${INPUT_FILE}\n\n" | tee -a ${LOG}
+    exit 1
+fi
+
+checkLineCount ${INPUT_FILE} ${SANITY_RPT} ${MIN_LINES}
+if [ $? -ne 0 ]
+then
+    echo "\nInput file has no data: ${INPUT_FILE}\n\n" | tee -a ${LOG}
+    exit 1
+fi
+
+checkColumns ${INPUT_FILE} ${SANITY_RPT} ${NUM_COLUMNS}
+if [ $? -ne 0 ]
+then
+    SANITY_ERROR=1
+fi
+
+checkDupLines ${INPUT_FILE} ${SANITY_RPT}
+if [ $? -ne 0 ]
+then
+    SANITY_ERROR=1
+fi
+
+checkActionValues ${INPUT_FILE} ${SANITY_RPT}
+if [ $? -ne 0 ]
+then
+    SANITY_ERROR=1
+fi
+
+if [ ${SANITY_ERROR} -ne 0 ]
+then
+    echo "Sanity errors detected. See ${SANITY_RPT}" | tee -a ${LOG}
+fi
+
+#${FEARLOAD}/bin/fearQC.py ${INPUT_FILE}
+#STAT=$?
+#if [ ${STAT} -eq 1 ]
+#then
+#    echo "Fatal initialization error. See ${SANITY_RPT}\n" | tee -a ${LOG}
+#    exit ${STAT}
+#fi
+
+#if [ ${STAT} -eq 3 ]
+#then
+#    echo "Fatal sanity errors detected. See ${SANITY_RPT}\n" | tee -a ${LOG}
+#else
+#    echo "No fatal sanity errors detected."
+#fi
+
+echo "" >> ${LOG}
+date >> ${LOG}
+echo "Finished running sanity checks on the input file" >> ${LOG}
+
+exit 0
