9f9c13c44358c2c063c0de7064db1e849654aaaa
Lori Corbani <lori.corbani@jax.org>
2016-09-01 08:17:16 -0400

    _Marker_Status_key not in (1,3) changed to = 2

--- bin/fearQC.py
+++ bin/fearQC.py
@@ -656,7 +656,7 @@ def qcOrgAllelePartMarker():
                 and a._LogicalDB_key = 1
                 and a._MGIType_key = t._MGIType_key
                 and a._Object_key = m._Marker_key
-                and m._Marker_Status_key not in (1,3)
+                and m._Marker_Status_key = 2
                 and m._Marker_Status_key = ms._Marker_Status_key
                 order by tmp.mgiID2''' % idTempTable
     print 'running sql for results2c %s ' % time.strftime("%H.%M.%S.%m.%d.%y", \
@@ -971,7 +971,7 @@ def qcOrgMarkerPartMarker():
 		and a._MGIType_key = 2
 		and a._MGIType_key = t._MGIType_key
                 and a._Object_key = m._Marker_key
-                and m._Marker_Status_key not in (1,3)
+                and m._Marker_Status_key = 2
                 and m._Marker_Status_key = ms._Marker_Status_key
                 order by tmp.mgiID1)''' % (idTempTable, idTempTable, idTempTable)
     #print cmds
@@ -1019,7 +1019,7 @@ def qcOrgMarkerPartMarker():
                 and a._LogicalDB_key = 1
                 and a._MGIType_key = t._MGIType_key
                 and a._Object_key = m._Marker_key
-                and m._Marker_Status_key not in (1,3)
+                and m._Marker_Status_key = 2
                 and m._Marker_Status_key = ms._Marker_Status_key
                 order by tmp.mgiID2)''' % (idTempTable, idTempTable, idTempTable)
     #print cmds
